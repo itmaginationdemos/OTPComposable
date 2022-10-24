ddd3a1e3a5979dc5f92ddb33d4dcb69a9bca9ce2
kkarlo <kkaragic@gmail.com>
2022-10-24 16:12:43 +0200

    Import lib in sample

--- app/src/main/java/com/sample/myapplication/MainActivity.kt
+++ app/src/main/java/com/sample/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ import com.sample.myapplication.ui.theme.OtpComposableTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             OtpComposableTheme {
                 // A surface container using the 'background' color from the theme
